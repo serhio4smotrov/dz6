d7255fcffd6eb75d7d074c37740fc8b7a86845cf
sergey <ser19061995@gmail.com>
2022-08-21 17:55:22 +0300

    подправил 3 задание

--- src/zad3.java
+++ src/zad3.java
@@ -1,7 +1,7 @@
 public class zad3 {
     public static void main(String[] args) {
         String fullName = "Ivanov Ivan Ivanovich";
-        fullName = fullName.replace(' ',';');
-        System.out.println("Данные ФИО сотрудника для административного отдела - "+ fullName);
+        fullName = fullName.replace(" ","; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела - "+fullName);
     }
 }
